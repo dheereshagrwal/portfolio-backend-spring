779e9afe30eb033622d190fbdc82d961600ce450
Dheeresh Agarwal <da11@iitbbs.ac.in>
2023-08-30 14:58:49 +0530

    cleaned

--- src/main/java/com/dheereshagrwal/portfolio/contact/ContactForm.java
+++ src/main/java/com/dheereshagrwal/portfolio/contact/ContactForm.java
@@ -5,8 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 @Entity public class ContactForm {
     @Id @GeneratedValue private int id;
